b4b81372ab0e94bb7bf79a94377c3cb098e31336
Pablo Llorens <pllomuo@teleco.upv.es>
2025-03-14 18:20:40 +0100

    Algunas páginas sin router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hola mundo</h1>
+  <DomusPagina/>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DomusPagina from './modulorum/landing/paginae/DomusPagina.vue';
+
+
+
+</script>
 
 <style scoped></style>
---
+++ src/modulorum/landing/paginae/PeculiaritatesPagina copy.vue
@@ -0,0 +1,6 @@
+
+<template>
+    
+    <h1>Features Page</h1>
+
+</template>
