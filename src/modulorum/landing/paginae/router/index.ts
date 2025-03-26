df890b8f1867cb29cc6bb264d1bff7329ce93600
Pablo Llorens <pllomuo@teleco.upv.es>
2025-03-26 19:34:10 +0100

    Navegacion con router

---
+++ src/modulorum/landing/paginae/router/index.ts
@@ -0,0 +1,30 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import DomusPagina from '../DomusPagina.vue';
+
+export const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: DomusPagina,
+    },
+    {
+      path: '/features',
+      name: 'features',
+      component: () => import('@/modulorum/landing/paginae/PeculiaritatesPagina.vue'),
+    },
+    {
+      path: '/pricing',
+      name: 'pricing',
+      component: () => import('@/modulorum/landing/paginae/PretiumPagina.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('@/modulorum/landing/paginae/ContactusPagina.vue'),
+    },
+  ],
+});
+
+export default router;
