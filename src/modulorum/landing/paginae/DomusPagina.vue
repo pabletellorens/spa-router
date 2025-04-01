fdb1c346ec0176f3d0d014aab154866e20261839
Pablo Llorens <pllomuo@teleco.upv.es>
2025-04-01 17:58:43 +0200

    Ciclo de vida, link active, keep alive

--- src/modulorum/landing/paginae/DomusPagina.vue
+++ src/modulorum/landing/paginae/DomusPagina.vue
@@ -5,5 +5,11 @@
     <p class="md:text-xl text-gray-500 mx-auto w-[600px]">
       Donum mox recipies pro dielitate erga fructus nostros
     </p>
+
+    <p>Contador: {{ numerator }}</p>
+
+    <button @click="numerator++">+1</button>
   </div>
 </template>
+
+<script lang="ts" src="./DomusPagina.ts"></script>
